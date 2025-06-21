141965150125ac944e0385c5ab073118382c9d96
JACK LIN <cvse00566@gmail.com>
2025-06-21 21:30:23 +0800

    調整分頁樣式

--- resources/views/pages/post/post-list.blade.php
+++ resources/views/pages/post/post-list.blade.php
@@ -71,8 +71,8 @@
                 @endforelse
 
                 <!-- 分頁 -->
-                <div class="mt-4">
-                    {{ $posts->links() }}
+                <div class="d-flex justify-content-center mt-4">
+                    {{ $posts->links('pagination::bootstrap-5') }}
                 </div>
             </section>
         </div>
